bc1c1e03d6a5c52ce19f3a7c4f02dd537669cd63
Abinesh <abineshferrari129@gmail.com>
2026-02-15 08:15:40 +0530

    Minor Changes

--- Frontend/myscada/src/asserts/components/URL.jsx
+++ Frontend/myscada/src/asserts/components/URL.jsx
@@ -1,5 +1,5 @@
-const BURL = "http://10.143.23.166:8000/";//when mobile hotspot
-//const BURL = "http://localhost:8000/";
+//const BURL = "http://10.143.23.166:8000/";//when mobile hotspot
+const BURL = "http://localhost:8000/";
 //const BURL = "http://10.81.47.166:8000/";
 //const BURL = "http://192.168.0.101:8000/";
 
