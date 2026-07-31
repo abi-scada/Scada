6abcebcaa2f3da2d86e2778a865c5151abf4e684
Abinesh M <abineshferrari129@gmail.com>
2026-07-31 21:32:23 +0530

    avien mysql db added

--- Backend/database.py
+++ Backend/database.py
@@ -1,7 +1,14 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+from pathlib import Path
+import os 
 
-db_url = "mysql://avnadmin:AVNS_qplbrG0Ox0nm1j4ZIg-@mysql-abiscada-abiscada143.c.aivencloud.com:12742/defaultdb"
+
+ENV_PATH = Path(__file__).resolve().parent / ".env"
+load_dotenv(ENV_PATH)  # Load .env file from Backend folder
+
+db_url = f"mysql://avnadmin:{os.getenv("DB_PASSWORD")}@mysql-abiscada-abiscada143.c.aivencloud.com:12742/defaultdb"
 
 engine = create_engine(db_url)
 SL = sessionmaker(autocommit=False, autoflush=False,bind = engine)
